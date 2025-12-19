82d28f142acdcf7eb5643cd2eb4fa3d51869e4e1
ankou <fabienankou25@gmail.com>
2025-12-19 15:34:04 +0100

    init

---
+++ src/main/java/com/example/springweave/SpringWeaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.springweave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeaveApplication.class, args);
+    }
+
+}
